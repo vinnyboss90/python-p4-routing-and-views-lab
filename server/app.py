6478712229c3dc01f6525c2c7ccf99c21b3e024a
vinnyegon90 <vinnyegon90@gmail.com>
2024-04-04 11:13:46 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -1,43 +1,47 @@
+#!/usr/bin/env python3
+
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return '<h1>Python Operations with Flask Routing and Views</h1>'
+    return '<h1>Python Operations with Flask Routing and Views</h1>' ,200
 
-@app.route('/print/<string:param>')
-def print_string(param):
-    print(param)
-    return param
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return parameter
 
-@app.route('/count/<int:param>')
-def count(param):
-    numbers = '\n'.join(str(i) for i in range(param))  # Start from 0 and go up to param - 1
-    numbers += '\n'  # Add a trailing newline character
-    return numbers
+# @app.route('/count/<int:parameter>')
+# def count(parameter):
+#     numbers = ''
+#     for num in range(1, parameter + 1):
+#         numbers += str(num) + '\n'
+#     return numbers 
 
-@app.route('/math/<int:num1>/<operation>/<int:num2>')
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    numbers = ''
+    for num in range(parameter):  # Start the range from 0
+        numbers += str(num) + '\n'
+    return numbers, 200
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
 def math(num1, operation, num2):
     result = None
     if operation == '+':
-        result = num1 + num2
+        result = num1 + num2, 200
     elif operation == '-':
-        result = num1 - num2
+        result = num1 - num2, 200
     elif operation == '*':
-        result = num1 * num2
+        result = num1 * num2, 200
     elif operation == 'div':
-        if num2 != 0:
-            result = num1 / num2
-        else:
-            return 'Division by zero is not allowed'
+        result = num1 / num2, 200
     elif operation == '%':
-        result = num1 % num2
-
+        result = num1 % num2, 200 
     if result is not None:
-        return str(result)  # Return only the result as a string
+        return str(result)
     else:
-        return 'Invalid operation'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+        return None
